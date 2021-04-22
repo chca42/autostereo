35986dc735128e4a37dd889ea2f87588e50d3398
Christian Carlowitz <cc@cmesh.de>
2021-04-22 22:23:26 +0200

    initial commit

---
+++ autostereo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import random
+import math
+import Image
+
+imPattern = Image.open("pattern.jpg")
+imPatternSize = imPattern.size
+
+imDepth = Image.open("depth.jpg")
+imDepthSize = imDepth.size
+factorY = imPatternSize[1] / float(imDepthSize[1])
+imDepthSize = (int(imDepthSize[0]*factorY), int(imDepthSize[1]*factorY))
+imDepth = imDepth.resize(imDepthSize, Image.ANTIALIAS)
+
+size = ( imDepthSize[0], imPatternSize[1] )
+im = Image.new("RGB", size)
+
+stripWidth = imPatternSize[0]
+
+for i in range(0,size[0]):
+	for j in range(0,size[1]):
+		if i < stripWidth:
+			colour = imPattern.getpixel((i,j))
+			im.putpixel((i,j), colour)
+		elif i > stripWidth and (i-stripWidth) < imDepthSize[0] and j < imDepthSize[1]:
+			height = int(imDepth.getpixel((i-stripWidth,j))/255.0*(imPatternSize[0]/2))
+			colour = im.getpixel((i-stripWidth+height, j))
+		else:
+			colour = im.getpixel((i-stripWidth, j))
+		im.putpixel((i,j), colour)
+
+im.show()
+
